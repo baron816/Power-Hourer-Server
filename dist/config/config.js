22be651530b49d3fbc0bbbde0d8e310e9c28a047
Baron Willeford <baron.willeford@gmail.com>
2017-05-31 17:17:46 -0500

    fixed build

--- dist/config/config.js
+++ dist/config/config.js
@@ -16,12 +16,12 @@ var _bluebird2 = _interopRequireDefault(_bluebird);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8000
 };
 
 
 _mongoose2.default.Promise = _bluebird2.default;
-_mongoose2.default.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/power-hourer');
+_mongoose2.default.connect(process.env.MONGODB_URI);
 
 exports.mongoose = _mongoose2.default;
 //# sourceMappingURL=config.js.map
