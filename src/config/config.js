1381f6b435dbe9c0135f2dcbd5f8f5bd0febac52
Baron Willeford <baron.willeford@gmail.com>
2017-06-04 15:39:44 -0500

    lengthens expiretime

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ import bluebird from 'bluebird';
 
 export default {
   port: process.env.PORT || 8000,
-  expireTime: 24 * 60 * 10,
+  expireTime: 24 * 60 * 90,
   secret: process.env.JWT_SECRET
 };
 
